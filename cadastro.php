f741e4d0414f7e3f42b518232d2bceb665396671
LARA SAYURI SILVA TURATO HASHIMOTO <lara.ssthashimoto@senacsp.edu.br>
2025-09-29 11:05:11 -0300

    primeiro commit

---
+++ cadastro.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fashion Nails</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Unhas com Glamour</h1>
+    <form action="áreadocliente.php" method="post">
+    <div class="row container justify-content-center" >
+        <div class="col-stat-2 col md-1" >  
+            <input type="text" name="nome" id="" class="form-control" placeholder="Digite seu nome...">
+        </div>
+        <div class="row" >
+            <div class="col md-3">  
+                <input type="email" name="email" id="" class="form-control" placeholder="Digite seu email...">
+            </div>
+        <div class="row">
+            <div class="col-md-3">
+                <input type="password" name="senha" id="" class="form-control" placeholder="Digite sua senha...">
+            </div>
+            <div class="col-md-3">
+                <input type="password" name="confirmarsenha" id="" class="form-control" placeholder="Confirme sua senha...">
+            </div>
+            <div class="row md-3 text-center">
+                    <div class="col-md-12">
+                        <input type="submit" class="btn btn-primary btn-lg" value="Cadastrar">
+                    </div>
+            </div>
+        </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ início.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fashion Nails</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+<body>
+    <h1>Bem Vindo(a)!</h1>
+    <div id="carouselExampleFade" class="carousel slide carousel-fade ">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/unhas4.png" class="d-block w-1920 h-750"  alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/unhas5.jpg" class="d-block w-1920 h-750" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/unhas6.webp" class="d-block w-1920 h-750" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
